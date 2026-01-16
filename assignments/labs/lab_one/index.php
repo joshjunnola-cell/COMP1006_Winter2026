98755e2902b902697881d77cd43b62b5c3ad59cc
Josh Junnola <joshjunnola@gmail.com>
2026-01-16 15:39:05 -0500

    answered last question

--- assignments/labs/lab_one/index.php
+++ assignments/labs/lab_one/index.php
@@ -6,3 +6,8 @@ require "connect.php"; //database connection/test
 require "car.php"; 
 echo "<p> Follow the instructions outlined in instructions.txt to complete this lab. Good luck & have fun!😀 </p>";
 require "footer.php"; 
+
+// REFLECTION:
+// What was the most challenging part of this lab?
+// I found the most challenging part of this lab was making sure I was using proper syntax and structure for PHP as it does differ from other programming languages I have used before. I did find it a bit confusing as to how to connect each file to the index.php file at first but that was because I was in the wrong directory.
+// I found it fairly easy to create classes and objects, just had to make sure I was following the correct format.
