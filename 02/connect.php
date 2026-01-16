f9eabbcf17a084ee4de40294e4a91ca18be60381
Josh Junnola <joshjunnola@gmail.com>
2026-01-16 14:03:39 -0500

    lesson 2 complete

--- 02/connect.php
+++ 02/connect.php
@@ -1,19 +1,21 @@
-<?php 
-
+<?php
 declare(strict_types=1);
 
-$host = "localhost"; 
-$db = "test_connection"; 
-$user = "root"; 
-$pass = ""; 
+$host = 'localhost'; //hostname
+$db = "week2"; //database name
+$user = "root"; //username
+$password = ""; //password
 
+//Points to the database
 $dsn = "mysql:host=$host;dbname=$db";
 
+//try to connect to the database, if connected echo yay
 try {
- $pdo = new PDO($dsn, $user, $pass); 
- $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
- echo "Connected to the database! Yay!"; 
+    $pdo = new PDO ($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "<p> Yay, Connected to the database! </p>";
 }
-catch (PDOException $e) {
+//what happens if there is an error connecting to the database
+catch(PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
