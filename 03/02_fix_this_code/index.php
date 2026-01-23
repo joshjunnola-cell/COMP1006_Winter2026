7589f1b7cc87bbc99e6ceaf87aef8ee36ca714ca
Josh Junnola <joshjunnola@gmail.com>
2026-01-23 13:36:02 -0500

    fix

--- 03/02_fix_this_code/index.php
+++ 03/02_fix_this_code/index.php
@@ -4,18 +4,17 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 
-$host = "localhost"
-
-$dbname = "week_two";
+$host = "localhost";
+$db = "week_two";
 $username = "root";
 $password = "";
 
-$dsn = "mysql:host=$hostdbname=$dbname";
+$dsn = "mysql:host=$host;dbname=$db";
 
 try {
     
     $pdo = new PDO($dsn $username,);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE PDO::ERRMODE_SILENT);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
 
     echo "Connected to database!";
 
